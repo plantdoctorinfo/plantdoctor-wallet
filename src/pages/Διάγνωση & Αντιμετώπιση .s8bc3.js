b4fcd03b7ff2693d35df02ca79952920caf8f169
Manolis Zaraidonis <manoliszaraidonis@Mac.home>
2026-06-24 02:06:31 +0300

    Fix image url upload

--- src/pages/Διάγνωση & Αντιμετώπιση .s8bc3.js
+++ src/pages/Διάγνωση & Αντιμετώπιση .s8bc3.js
@@ -8,9 +8,14 @@ $w.onReady(function () {
 
         try {
             const uploadedFiles = await $w("#uploadPlantImage").uploadFiles();
-            const imageUrl = uploadedFiles[0].fileUrl;
 
-            const result = await testDiagnosis(symptoms, imageUrl);
+            const imageUrl = uploadedFiles[0]?.fileUrl;
+
+              if (!imageUrl) {
+              throw new Error("Δεν βρέθηκε φωτογραφία");
+              }
+
+                const result = await testDiagnosis(symptoms, imageUrl);
             $w('#resultText').text =
                 result + "\n\nΠεριγραφή: " + symptoms;
         } catch (error) {
